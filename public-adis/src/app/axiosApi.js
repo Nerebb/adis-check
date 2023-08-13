1d3b125f32956485b2f992fdcf875781069d8b0f
tuan-zigvy <139092565+tuan-zigvy@users.noreply.github.com>
2023-08-13 10:25:43 +0700

    feat/fe-homepage

--- public-adis/src/app/axiosApi.js
+++ public-adis/src/app/axiosApi.js
@@ -16,6 +16,55 @@ const axiosApi = {
     const url = BASE_API + "/user/recover";
     return axiosClient.post(url, { email });
   },
+  getAdsBySearch: (q) => {
+    const url = BASE_API + "/ads";
+    return axiosClient.get(url, { params: { ...q } });
+  },
+  getAdsByCategory: (id) => {
+    const url = BASE_API + `/ads/category/${id}`;
+    return axiosClient.get(url);
+  },
+  getDetail: (id) => {
+    const url = BASE_API + `/ads/detail/${id}`;
+    return axiosClient.get(url);
+  },
+  createAds: (data) => {
+    const url = BASE_API + `/ads`;
+
+    return axiosClient.post(url, {
+      data,
+    });
+  },
+
+  updateAds: (id, data) => {
+    const url = BASE_API + `/ads/${id}`;
+
+    return axiosClient.put(url, {
+      data,
+    });
+  },
+  updateStatus: (id, data) => {
+    const url = BASE_API + `/ads/status/${id}`;
+    return axiosClient.patch(url, {
+      data,
+    });
+  },
+  getCountries: (q) => {
+    const url = BASE_API + "/ads/location/countries";
+    return axiosClient.get(url, { params: { q } });
+  },
+  getCites: (id) => {
+    const url = BASE_API + `/ads/location/cites/${id}`;
+    return axiosClient.get(url);
+  },
+  getStates: (id) => {
+    const url = BASE_API + `/ads/location/states/${id}`;
+    return axiosClient.get(url);
+  },
+  getCategory: () => {
+    const url = BASE_API + `/category`;
+    return axiosClient.get(url);
+  },
 };
 
 export default axiosApi;
